aacf18cf7fdf1a566dea670a0d4af774372ae8d7
miaoles <iao_mm@pm.me>
2021-10-01 23:30:09 -0400

    updates

--- home.nix
+++ home.nix
@@ -1,38 +1,43 @@
 # iao NixOS home-manager configuration
 
-{ config, pkgs, lib }:
+{ config, pkgs, lib, ... }:
 
 {
-  # Imports
-  imports = [
+	imports = [];
 
-  ];
+	home = {
+		username = "miles";
+		homeDirectory = "/home/miles";
 
-  # Let home-manager manage itself.
-  programs.home-manager.enable = true;
-  # Allow unfree packages.
-  nixpkgs.config.allowUnfree = true;
+		# Install these packages for just this user.
+		packages = with pkgs; [
+			cmst
+			chatterino2
+			copyq
+			discord
+			firefox
+			picom
+			tint2
+		];
 
-  # Install these packages for just this user.
-  home.packages = with pkgs; [];
+		sessionVariables = {};
 
-  # Include man-pages.
-  manual.manpages.enable = true;
+		# This value determines the Home Manager release that your
+		# configuration is compatible with. This helps avoid breakage
+		# when a new Home Manager release introduces backwards
+		# incompatible changes.
+		#
+		# You can update Home Manager without changing this value. See
+		# the Home Manager release notes for a list of state version
+		# changes in each release.
+		stateVersion = "21.05";
+	}
 
-  # Environment variables.
-  systemd.user.sessionVariables = {};
+	# Let home-manager manage itself.
+	programs.home-manager.enable = true;
+	# Allow unfree packages.
+	nixpkgs.config.allowUnfree = true;
 
-  home.sessionVariables = {};
+	services.picom.enable = true;
 
-  services = {};
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "21.05";
 }
